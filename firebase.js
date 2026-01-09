2f1c97f0f82d092078e36c4b54021c62ebf8de1e
CARLOS ALEJANDRO GARCIA <carlosale.miranda555@gmail.com>
2026-01-09 15:16:41 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,20 @@
-// firebase.js
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-database-compat.js"></script>
+
+<script>
 const firebaseConfig = {
   apiKey: "AIzaSyAfZ0WZVzMxUX2bWmP-iJ0SB-r4q9r026g",
   authDomain: "sucursal-ga.firebaseapp.com",
+  databaseURL: "https://sucursal-ga-default-rtdb.firebaseio.com",
   projectId: "sucursal-ga",
-  storageBucket: "sucursal-ga.firebasestorage.app",
+  storageBucket: "sucursal-ga.appspot.com",
   messagingSenderId: "692794208494",
   appId: "1:692794208494:web:9ef044edad3263926943ff"
 };
 
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+window.database = firebase.database();
+</script>
