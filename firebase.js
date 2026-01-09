5f5b25aacb74b23eed4f6dcb6e428bd713fea030
CARLOS ALEJANDRO GARCIA <carlosale.miranda555@gmail.com>
2026-01-09 14:24:38 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAfZ0WZVzMxUX2bWmP-iJ0SB-r4q9r026g",
+  authDomain: "sucursal-ga.firebaseapp.com",
+  projectId: "sucursal-ga",
+  storageBucket: "sucursal-ga.firebasestorage.app",
+  messagingSenderId: "692794208494",
+  appId: "1:692794208494:web:9ef044edad3263926943ff"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
